cee65ef3f7545fc312feaed207a16f97bc500a7c
schreibertuc <156848017+schreibertuc@users.noreply.github.com>
2025-07-05 15:42:33 -0400

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,7 +1,7 @@
 // supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://your-project.supabase.co'; // replace
-const supabaseKey = 'your-anon-key'; // replace
+const supabaseUrl = 'https://wgbldbyohrxjdlkglryp.supabase.co'; // replace
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndnYmxkYnlvaHJ4amRsa2dscnlwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTE3MzkyNDksImV4cCI6MjA2NzMxNTI0OX0.g839T_i_CJq66GvUXZHTgq7n_f6kqe58-tnvhtYXydY'; // replace
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
